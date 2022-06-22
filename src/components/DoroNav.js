95124047d71b5cfc61844d78fbf051ef1e100813
Mohacyr Rojas <mohacyr.rojas.contractor@bbva.com>
2022-06-22 18:14:49 -0500

    feat(ux): add navbar styles

--- src/components/DoroNav.js
+++ src/components/DoroNav.js
@@ -10,10 +10,11 @@ export class DoroNav extends LitElement {
   static get styles() {
     return css`
       nav {
-        background-color: #171f26;
+        background-color: var(--nav-bgcolor);
       }
 
       ul {
+        margin: 0;
         padding: 0 4rem;
         display: flex;
         align-items: center;
@@ -22,6 +23,18 @@ export class DoroNav extends LitElement {
 
       nav li {
         list-style: none;
+      }
+
+      nav li:first-child {
+        color: var(--nav-link);
+      }
+
+      nav li a {
+        color: var(--nav-link);
+      }
+      nav li a:hover {
+        color: var(--nav-link-active);
+        text-decoration: none;
       }
     `;
   }
@@ -38,7 +51,9 @@ export class DoroNav extends LitElement {
           <li>
             <h1>MohaDoro</h1>
           </li>
-          <li>GitHub</li>
+          <li>
+            <a href="https://github.com/MHCYR/MohaDoro"> GitHub </a>
+          </li>
         </ul>
       </nav>
     `;
