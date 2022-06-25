bbdc7dcab3a6d7a95c8befdae5162df8f0f8c763
Mohacyr Rojas <mohacyr.rojas.contractor@bbva.com>
2022-06-25 13:39:37 -0500

    feat(js): add countdown functionality

--- src/components/DoroTimer.js
+++ src/components/DoroTimer.js
@@ -19,16 +19,36 @@ export class DoroTimer extends LitElement {
 
   constructor() {
     super();
-    this.duration = 60;
+    this.duration = 63;
     this.end = null;
     this.remaining = 0;
   }
 
   render() {
+    const { remaining } = this;
+    const sec = Math.floor((remaining / 1000) % 60);
+    const min = Math.floor(remaining / 60000);
     return html`
       <div class="timer-wrapper">
-        <h1>Timer</h1>
+        <h1>${`${min}:${sec}`}</h1>
+        <button @click=${this.start}>Start</button>
       </div>
     `;
+  }
+
+  start() {
+    this.end = Date.now() + this.duration * 1000;
+    this.tick();
+  }
+
+  tick() {
+    this.remaining = this.end - Date.now();
+    requestAnimationFrame(() => {
+      this.tick();
+    });
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
   }
 }
